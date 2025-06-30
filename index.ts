ea84e1752e80298c4f4260e479c7134b9e59eaf2
Yuzu <Yuzu_0204@outlook.com>
2025-06-29 22:26:03 -0400

    feat(Setup): :tada: Add initial project files including configuration, README, and TypeScript setup

---
+++ index.ts
@@ -0,0 +1,13 @@
+type User = {
+	id: number;
+	name: string;
+	email: string;
+};
+
+const user1: User = {
+	id: 1,
+	name: "Yuzu",
+	email: "Yuzu_0204@outlook.com",
+};
+
+console.log(`User ID: ${user1.id}, Name: ${user1.name}, Email: ${user1.email}`);
